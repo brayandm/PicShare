9fdb2d15bb8ac49df50c64e8b721e8c31d83509d
Brayan Durán Medina <63823707+brayandm@users.noreply.github.com>
2022-11-12 21:36:33 +0100

    factories added

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'post_id' => \App\Models\Post::factory()->create(),
+            'text' => fake()->text(200),
+        ];
+    }
+}

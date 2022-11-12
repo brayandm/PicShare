22d47ccf2ea79544220e884fd5fee889afd49239
Brayan Durán Medina <63823707+brayandm@users.noreply.github.com>
2022-11-12 23:33:12 +0100

    dashboard changed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,16 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+            @foreach ($posts as $post)
+                <div class="mb-5 w-1/2 mx-auto bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <p class="mb-5 text-end">{{ $post->person->user->name }}</p>
+                        <h1 class="mb-10 text-xl">{{ $post->header }}</h1>
+                        <p class="mb-5">{{ $post->text }}</p>
+                        <p>Likes: {{ $post->likes }}</p>
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </x-app-layout>

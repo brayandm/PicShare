aedf01393a715c2ed89c1f31af8df1e0a0e502f2
Brayan Durán Medina <63823707+brayandm@users.noreply.github.com>
2022-11-13 00:30:55 +0100

    edit page added

---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Profile') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="mb-5 w-1/2 mx-auto bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="flex flex-col p-6 bg-white border-b border-gray-200">
+                    <h1 class="text-2xl mb-10"> {{ $person->user->name }}</h1>
+
+                    <form action={{ route('profile.edit') }} method="POST">
+
+                        @csrf
+                        @method('put')
+
+                        <div class="flex flex-col">
+                            <label for="description" class="text-xl">Description:</label><br>
+                            <textarea id="description" name="description" value="{{ $person->description }}" class="mb-5" rows="5"></textarea><br>
+
+                            <label for="birthdate" class="text-xl">Birthdate:</label><br>
+                            <input type="date" id="birthdate" name="birthdate" value="{{ $person->birthdate }}"
+                                class="mb-5"><br>
+
+                            <input type="submit" value="Submit" class="self-end border p-2 rounded-xl bg-gray-200">
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

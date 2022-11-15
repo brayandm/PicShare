bf03c4bc52aa03f847e9a6eb6b3c4d9abeceb2e9
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2022-11-15 09:07:49 +0100

    person_id added to comment

--- database/migrations/2022_11_12_002502_create_comments_table.php
+++ database/migrations/2022_11_12_002502_create_comments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('person_id')->nullable()->constrained()->onDelete('cascade');
             $table->foreignId('post_id')->nullable()->constrained()->onDelete('cascade');
             $table->foreignId('comment_id')->nullable()->constrained()->onDelete('cascade');
             $table->string('text');

ce871cc1ac85bb2450af3ede80b03ec7e2b72585
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2022-11-15 19:33:25 +0100

    date added to post and sorting it in desc order by update at

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,13 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             @if (!Auth::user()->is_admin)
+                @php
+                    $posts = collect($posts)->sortByDesc('updated_at');
+                @endphp
                 @foreach ($posts as $post)
                     <div class="mb-5 w-1/2 mx-auto bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 bg-white border-b border-gray-200">
+                            <p class="mb-5 text-end">{{ (new Datetime($post->created_at))->format('Y-m-d H:i')}}</p>
                             <p class="mb-5 text-end">{{ $post->person->user->name }}</p>
                             <h1 class="mb-10 text-xl">{{ $post->header }}</h1>
                             <p class="mb-5">{{ $post->text }}</p>

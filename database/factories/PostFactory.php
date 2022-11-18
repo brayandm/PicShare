c0d0d1df194ef0f982ffd81e82d2d3d9e443b90e
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2022-11-18 08:07:22 +0100

    Image faker in factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -19,7 +21,19 @@ class PostFactory extends Factory
         return [
             'header' => fake()->text(50),
             'text' => fake()->text(200),
+            'picture' => $this->fakePicture(),
             'likes' => 0,
         ];
+    }
+
+    public function fakePicture()
+    {
+        $picture = UploadedFile::fake()->image('dummy.jpg');
+
+        $filename = Storage::putFile('private/pictures/', $picture);
+
+        $dir = explode('/', $filename);
+
+        return end($dir);
     }
 }

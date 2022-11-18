63481f1314c6a84985851fa0a91410a07610a12f
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2022-11-18 07:39:35 +0100

    adding functions to post services to process pictures

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -3,8 +3,11 @@
 namespace App\Services;
 
 use App\Models\Post;
+use Exception;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class PostService
 {
@@ -36,7 +39,8 @@ class PostService
     public function create($fields)
     {
         $fields['person_id'] = Auth::user()->person->id;
-        Post::create($fields);
+
+        return Post::create($fields);
     }
 
     public function like($id)
@@ -83,5 +87,74 @@ class PostService
         } catch (\Exception $e) {
             DB::rollback();
         }
+    }
+
+    private function deletePicturePath(string $path)
+    {
+        if (! Storage::exists($path)) {
+            throw new Exception('Picture not found');
+        }
+
+        if (! Storage::delete($path)) {
+            throw new Exception('The picture could not be deleted from storage');
+        }
+    }
+
+    public function getPicturePath($id)
+    {
+        $filename = Post::find($id)->picture;
+
+        if ($filename != null) {
+            return Storage::path('private/pictures/'.$filename);
+        }
+
+        return null;
+    }
+
+    public function savePicture($id, UploadedFile $file)
+    {
+        $post = Post::find($id);
+
+        if ($post->picture != null) {
+            $this->deletePicturePath('private/pictures/'.$post->picture);
+
+            $post->picture = null;
+            $post->save();
+        }
+
+        $filename = Storage::putFile('private/pictures/', $file);
+
+        if ($filename === false) {
+            throw new Exception('The picture could not be saved to storage correctly');
+        }
+
+        $dir = explode('/', $filename);
+
+        $post->picture = end($dir);
+        $post->save();
+    }
+
+    public function existsPicture($id)
+    {
+        $filename = Post::find($id)->picture;
+
+        if ($filename != null) {
+            if (Storage::exists('private/pictures/'.$filename)) {
+                return true;
+            }
+            throw new Exception('The picture could not be found in the storage');
+        }
+
+        return false;
+    }
+
+    public function deletePicture($id)
+    {
+        $post = Post::find($id);
+
+        $this->deletePicturePath('private/pictures/'.$post->picture);
+
+        $post->picture = null;
+        $post->save();
     }
 }

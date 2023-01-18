5c2965b56069dc79e4dce3b7f961772c1aebfb21
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-01-18 01:08:33 +0100

    weather service added

---
+++ app/Services/WeatherService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+
+class WeatherService
+{
+    private $secret;
+    private $baseUrl;
+    private $client;
+
+    public function __construct()
+    {
+        $this->secret = config('weatherservice.secret');
+        $this->baseUrl = config('weatherservice.url');
+        $this->client = new Client();
+    }
+
+    private function request($endpoint, $query)
+    {
+        $request = $this->client->get($this->baseUrl . '/' . $endpoint . '.json', [
+            RequestOptions::QUERY => [
+                'key' => $this->secret,
+                'q' => $query,
+            ],
+            'accept' => 'application/json'
+        ]);
+
+        return json_decode($request->getBody());
+    }
+
+    public function temperature($query)
+    {
+        return $this->request('current', $query)->current->temp_c;
+    }
+}

258eb38459bdb879fcc1d5c4d33e0ebfc83f2956
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-01-23 10:38:19 +0100

    update un premium account

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     @if (!Auth::user()->person->is_premium)
                         <a class="mb-5 text-end text-red-500 underline text-xl" href={{ route('premium.buy') }}>
-                            <p> Want to have Premium
+                            <p> Want to have a Premium
                                 Account?</p>
                         </a>
                     @else

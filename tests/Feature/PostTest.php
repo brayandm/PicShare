e786500dc203bb3366c168cfc9f96581c688904b
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-01-23 19:39:33 +0100

    testGetMyPosts added

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
@@ -12,5 +13,23 @@ class PostTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
+    }
+
+    public function testGetMyPosts()
+    {
+        $this->actingAs($this->exampleUser);
+
+        $post = Post::factory()->create([
+            'header' => 'This is a header',
+            'text' => 'This is a text',
+            'person_id' => $this->examplePerson->id,
+        ]);
+
+        $response = $this->get(route('myposts.show'));
+
+        $response->assertSee('myposts');
+        $response->assertSee($post->header);
+        $response->assertSee($post->text);
+        $response->assertSee($this->exampleUser->name);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,32 @@
 
 namespace Tests;
 
+use App\Models\Person;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
+    protected $exampleUser;
+    protected $examplePerson;
+
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->exampleUser = User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        $this->examplePerson = Person::where('user_id', $this->exampleUser->id)->first();
+
+        $this->examplePerson->update([
+            'is_premium' => true,
+            'description' => 'This is a example person',
+            'birthdate' => '2000-1-1'
+        ]);
     }
 }

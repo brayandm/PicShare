3552d85ad3278bc54eaaf6cb0392f94ed20bc4b5
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-01-23 20:17:24 +0100

    testWelcomePageLogIn added

--- tests/Feature/WelcomePageTest.php
+++ tests/Feature/WelcomePageTest.php
@@ -22,5 +22,20 @@ class WelcomePageTest extends TestCase
         //ASSERTION
         $response->assertSee('Log in');
         $response->assertSee('Register');
+        $response->assertDontSee('Dashboard');
+    }
+
+    public function testWelcomePageLogIn()
+    {
+        //PREPARATION
+        $this->actingAs($this->exampleUser);
+
+        //EXECUTION
+        $response = $this->get(route('welcome'));
+
+        //ASSERTION
+        $response->assertDontSee('Log in');
+        $response->assertDontSee('Register');
+        $response->assertSee('Dashboard');
     }
 }

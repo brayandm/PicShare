cbca133b0521cbe153efb1a3d3c82b8a5e6758ee
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-01-23 19:09:24 +0100

    PostTest added

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}

c08bf2cb821d0b1be074cefe910c1330dfa625aa
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-01-25 15:02:24 +0100

    Api docs added

--- routes/api.php
+++ routes/api.php
@@ -24,10 +24,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function(){
     Route::middleware(['auth:sanctum'])->group(function(){
+        Route::post('/myposts/index', [PostController::class, 'index'])->name('api.myposts.index');
+        Route::post('/myposts/get/{id}', [PostController::class, 'get'])->name('api.myposts.get');
         Route::post('/myposts/create', [PostController::class, 'store'])->name('api.myposts.store');
         Route::post('/myposts/update/{id}', [PostController::class, 'update'])->name('api.myposts.update');
-        Route::post('/myposts/get/{id}', [PostController::class, 'get'])->name('api.myposts.get');
-        Route::post('/myposts/index', [PostController::class, 'index'])->name('api.myposts.index');
         Route::post('/myposts/delete/{id}', [PostController::class, 'delete'])->name('api.myposts.delete');
     });
 });
